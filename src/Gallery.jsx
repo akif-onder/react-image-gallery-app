20429b1b088d60fea7700b7ffbdc6ddd6a53763f
akif-onder <akifonder@gmail.com>
2022-07-26 12:21:53 +0300

    files generated

---
+++ src/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Images from "./images";
+
+const Gallery = () => {
+  return (
+    <div className='gallery'>
+        
+    </div>
+  )
+}
+
+export default Gallery
