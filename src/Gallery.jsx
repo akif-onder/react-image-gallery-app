1b8c8ebb1ca7c394c1dda0606fcecf3e363c19d8
akif-onder <akifonder@gmail.com>
2022-07-26 14:41:58 +0300

    functions and states added. end of project

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Images from "./images";
 import './gallery.css';
+import { FaWindowClose  } from "react-icons/fa";
+
+
+
 
 const Gallery = () => {
+    const [model, setModel] = useState(false);
+    const [tempImgSrc, setTempImgSrc] = useState('');
+
+    const getImg = (imgSrc) => {
+        setTempImgSrc(imgSrc);
+        setModel(true);
+    }
+
   return (
+    <>
+    <div className={model ? 'model open' : 'model'}>
+        <img src={tempImgSrc} />
+        <FaWindowClose onClick={() => setModel(false)} className='x'/>
+    </div>
     <div className='gallery'>
         {Images.map((item, index)=>{
             return(
                 <div className="pics" key={index}>
-                    <img src={item.image} style={{width: '100%'}} />
+                    <img onClick={() => getImg(item.image)} src={item.image} style={{width: '100%'}} />
                 </div>
             )
         })}
     </div>
+    </>
   )
 }
 
